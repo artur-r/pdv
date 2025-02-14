34c9cf524c4161daca3e76814095d98c9e116695
Artur Martins <arturmbh@gmail.com>
2025-02-14 16:33:37 -0300

    Tela categoria pronta

--- pages/categorias.php
+++ pages/categorias.php
@@ -35,7 +35,7 @@ else if (isset($_GET['search'])) {
 
 <body>
 
-    <div class="d-flex justify-content w-100 p-2">
+    <div class="w-100 p-2">
 
         <div class="bg-light w-100 vh-98 border rounded border-opacity-75 border-info">
             <div class="bg-body-secondary form-control box-search ">
@@ -47,19 +47,20 @@ else if (isset($_GET['search'])) {
                     Pesquisar
                 </button>
                 <a href="cadastroProduto.php" class="btn btn-success">
-                    Adicionar novo
+                    Adicionar nova
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                     </svg>
                 </a>
             </div>
-            <div class="p-3">
-                <table class="table table-striped -3 table-hover border  ">
-                    <thead class="table table-primary">
+            <div class="p-3 d-grid">
+                <table class="table table-responsive table-striped  table-hover border w-100 ">
+                    <thead class="table-primary">
                         <tr>
-                            <th>Código</th>
-                            <th>Nome da categoria</th>
+                            <th class="col">Código</th>
+                            <th class="col">Nome da categoria</th>
+                            <th class="col-1 text-center">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -68,12 +69,19 @@ else if (isset($_GET['search'])) {
                                 echo "<tr>
                                         <td>" . $result['id'] . "</td>
                                         <td>" . $result['nome'] . "</td>
+                                        <td class='text-center'>
+                                            <button class='btn btn-primary'>
+                                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                                                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325'/></svg>
+                                            </button> 
+                                            <button class='btn btn-danger'>
+                                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash3' viewBox='0 0 16 16'>
+                                                <path d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5'/></svg>
+                                             </button> 
+                                        </td>
                                      </tr>";
                             }
                         }
-
-
-
                         ?>
                     </tbody>
                 </table>
@@ -113,6 +121,8 @@ else if (isset($_GET['search'])) {
     function searchData() {
         window.location = 'categorias.php?search=' + search.value;
     }
+
+    
 </script>
 
 </html>
