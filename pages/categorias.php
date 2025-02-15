ba824248fdfc316a817715299213319bcb24e491
Artur Martins <arturmbh@gmail.com>
2025-02-15 15:12:44 -0300

    Deletar categoria

--- pages/categorias.php
+++ pages/categorias.php
@@ -28,7 +28,19 @@ if (!empty($_GET['id']) && !empty($_GET['nomeNovo'])) {
     ORDER BY id";
 
     $consulta = mysqli_query($conn, $sql);
+}
 
+if (!empty($_GET['deletar'])){
+    $id = $_GET['deletar'];
+    $sql = "DELETE FROM categoria  WHERE id = $id";
+
+    $atualiza = mysqli_query($conn, $sql);
+
+    
+    $sql = "SELECT * FROM categoria
+    ORDER BY id";
+
+    $consulta = mysqli_query($conn, $sql);
 }
 
 ?>
@@ -80,12 +92,12 @@ if (!empty($_GET['id']) && !empty($_GET['nomeNovo'])) {
                                 echo "<tr>
                                         <td>" . $result['id'] . "</td>
                                         <td>" . $result['nome'] . "</td>
-                                        <td class='text-center' data-bs-target='#modalEditar' data-bs-toggle='modal''>
-                                            <button class='btn btn-primary edit_button' data-id='" . $result['id'] . "'>
+                                        <td class='text-center'>
+                                            <button class='btn btn-primary action-button' data-bs-target='#modalEditar' data-bs-toggle='modal'data-id='" . $result['id'] . "'>
                                                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
                                                 <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325'/></svg>
                                             </button> 
-                                            <button class='btn btn-danger'>
+                                            <button class='btn btn-danger action-button' data-bs-target='#modalDeletar' data-bs-toggle='modal'data-id='" . $result['id'] . "'>
                                                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash3' viewBox='0 0 16 16'>
                                                 <path d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5'/></svg>
                                              </button> 
@@ -104,7 +116,7 @@ if (!empty($_GET['id']) && !empty($_GET['nomeNovo'])) {
 
 
 
-    <!-- Modal -->
+    <!-- Modal editar-->
     <div class="modal fade" id="modalEditar" tabindex="-1" aria-labelledby="modalEditarLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
@@ -119,13 +131,13 @@ if (!empty($_GET['id']) && !empty($_GET['nomeNovo'])) {
                     <div class="modal-body">
                         Insira o novo nome:
                         <div class="input-group">
-                            <input type="text" name="nome" id="nomeNovo" class="form-control input-group" required>
+                            <input type="text" name="nome" id="inputNome" class="form-control input-group" required>
                         </div>
                     </div>
 
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" onclick="atualizaNome()" class="btn btn-primary">Atualizar</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" onclick="atualizarNome()" class="btn btn-primary">Atualizar</button>
                     </div>
 
                 </form>
@@ -133,6 +145,31 @@ if (!empty($_GET['id']) && !empty($_GET['nomeNovo'])) {
         </div>
     </div>
 
+    <!-- Modal deletar-->
+    <<div class="modal fade" id="modalDeletar" tabindex="-1" aria-labelledby="modalDeletarLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <form action="" method="post">
+
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="modalDeletarLabel">Deletar categoria</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        Confirme se deseja deletar a categoria
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" id ="deleteButton" onclick="deletarCategoria()" class="btn btn-warning">Deletar</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div> 
 
 
 
@@ -167,18 +204,27 @@ if (!empty($_GET['id']) && !empty($_GET['nomeNovo'])) {
         window.location = 'categorias.php?search=' + search.value;
     }
 
-    document.querySelectorAll('.edit_button').forEach(button => {
-    button.addEventListener('click', function() {
-        var id = this.getAttribute('data-id'); // Captura o ID correto
-        document.getElementById('nomeNovo').setAttribute('data-id', id); // Armazena o ID no campo de input
+    //Evento de clique que armazena o id dentro do input (para o conteúdo ir para a URL, deve estar dentro do input)
+    document.querySelectorAll('.action-button').forEach(button => {
+        button.addEventListener('click', function() {
+            var id = this.getAttribute('data-id'); // Captura o ID correto
+            document.getElementById('inputNome').setAttribute('data-id', id); // Armazena o ID no campo de input para atualizar
+            document.getElementById('deleteButton').setAttribute('data-id', id); // Armazena o ID no botão de deletar
+        });
     });
-});
 
-function atualizaNome() {
-    var id = document.getElementById('nomeNovo').getAttribute('data-id'); // Recupera o ID salvo
-    var nomeNovo = document.getElementById('nomeNovo').value;
-    window.location = 'categorias.php?id=' + id + '&nomeNovo=' + encodeURIComponent(nomeNovo);
-}
+    //Função para atualizar o nome ao clicar no botão
+    function atualizarNome() {
+        var id = document.getElementById('inputNome').getAttribute('data-id'); // Recupera o ID salvo
+        var nomeNovo = document.getElementById('inputNome').value;
+        window.location = 'categorias.php?id=' + id + '&nomeNovo=' + encodeURIComponent(nomeNovo);
+    }
+
+    function deletarCategoria() {
+        var id = document.getElementById('deleteButton').getAttribute('data-id'); // Recupera o ID salvo
+        window.location = 'categorias.php?deletar=' + id;
+    }
+
 </script>
 
 </html>
