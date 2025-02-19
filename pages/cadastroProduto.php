af6d7aa25d25b648fb948d0eed87a25d91987244
Artur Martins <arturmbh@gmail.com>
2025-02-19 19:36:40 -0300

    Implementação da versão do formulário para atualizar produtos

--- pages/cadastroProduto.php
+++ pages/cadastroProduto.php
@@ -19,6 +19,20 @@
     $sql = "SELECT * FROM categoria";
     $consulta = mysqli_query($conn, $sql);
 
+    //pegar dados enviados
+    if (!empty($_GET)) {
+        $id = $_GET['id'];
+        $nome = $_GET['nome'];
+        $codCategoria = $_GET['cod_categoria'];
+        $categoria = $_GET['categoria'];
+        $custo = str_replace(',', '.', $_GET['custo']);
+        $venda = str_replace(',', '.', $_GET['venda']);
+        $tipoVenda = $_GET['tipo'];
+        $estoque = $_GET['estoque'];
+        $codBarra = $_GET['codBarra'];
+
+
+    }
 
     ?>
 
@@ -29,18 +43,26 @@
                 <h2 class="font-h2">Cadastro de produto</h2>
             </div>
             <div class="m-3">
+                
+            <!-- Caso seja atualização será criado um formulário para atualizar. E se for cadastro será para cadastrar -->
+            <?php if (empty($_GET)) { 
+              echo" <form action='../function/cadastraProduto.php' method='post' class='row needs-validation' novalidate>";
+            }else{
+                echo" <form action='../function/atualizaProduto.php' method='post' class='row needs-validation' novalidate>";
+            }
+            ?>
                 <form action="../function/cadastraProduto.php" method="post" class="row needs-validation" novalidate>
                     <div class="col-4 mb-4">
                         <label for="nome" class="form-label" id="validationCustom03">Nome</label>
-                        <input type="text" name="nome" id="" class="form-control" required>
+                        <input type="text" name="nome" id="" class="form-control" required value="<?php if (!empty($_GET)) { echo $nome; }?>">
                         <div class="invalid-feedback">
                             Favor inserir o nome do produto
                         </div>
                     </div>
                     <div class="col-4 mb-4">
                         <label for="categoria" class="form-label">Categoria</label>
-                        <select name="categoria" class="form-select  form-control " aria-label="Default select example" required>
-                            <option selected disabled value="">Selecione...</option>
+                        <select name="categoria" class="form-select  form-control " aria-label="Default select example" required >
+                            <option selected disabled value="<?php if (!empty($_GET)) { echo $codCategoria; }?>"><?php if (!empty($_GET)) { echo $categoria; }?></option>
 
                             <?php
                             while ($result = mysqli_fetch_assoc($consulta)) {
@@ -56,14 +78,14 @@
                     </div>
                     <div class="col-4 mb-4">
                         <label for="custo" class="form-label">Preço de custo</label>
-                        <input type="text" inputmode="decimal" pattern="^\d+([.,]\d{1,2})?$" name="custo" id="validationCustom03" class="form-control" required>
+                        <input type="text" inputmode="decimal" pattern="^\d+([.,]\d{1,2})?$" name="custo" id="validationCustom03" class="form-control" required value="<?php if (!empty($_GET)) { echo $custo; }?>">
                         <div class="invalid-feedback">
                             Insira um valor válido
                         </div>
                     </div>
                     <div class="col-4 mb-4">
                         <label for="venda" class="form-label">Preço de venda</label>
-                        <input type="text" inputmode="decimal" pattern="^\d+([.,]\d{1,2})?$" name="venda" id="" class="form-control" required>
+                        <input type="text" inputmode="decimal" pattern="^\d+([.,]\d{1,2})?$" name="venda" id="" class="form-control" required value="<?php if (!empty($_GET)) { echo $venda; }?>">
                         <div class="invalid-feedback">
                             Insira um valor válido
                         </div>
@@ -71,7 +93,7 @@
                     <div class="col-4 mb-4">
                         <label for="tipo" class="form-label">Tipo venda</label>
                         <select name="tipo" class="form-select  form-control " aria-label="Default select example" required>
-                            <option selected disabled value="">Selecione...</option>
+                            <option selected disabled value=""><?php if (!empty($_GET)) { echo $tipoVenda; }?></option>
                             <option value="unidade">Unidade</option>
                             <option value="real">Real</option>
                         </select>
@@ -81,21 +103,37 @@
                     </div>
                     <div class="col-4 mb-4">
                         <label for="estoque" class="form-label">Estoque</label>
-                        <input ttype="text" inputmode="decimal" pattern="^\d+([.,]\d{1,2})?$" name="estoque" id="" class="form-control">
+                        <input ttype="text" inputmode="decimal" pattern="^\d+([.,]\d{1,2})?$" name="estoque" id="" class="form-control"
+                        value="<?php if (!empty($_GET)) { echo $estoque; }?>">
                     </div>
                     <div class="col-4 mb-4">
                         <label for="cod" class="form-label">Cód. De barra</label>
-                        <input type="text" name="cod" id="" class="form-control">
+                        <input type="text" name="cod" id="" class="form-control" value="<?php if (!empty($_GET)) { echo $codBarra; }?>">
                     </div><br>
                     <div class="">
-                        <button type="submit" name="cadastrar" id="" class="btn btn-success ">
+                        <?php if (!empty($_GET)){
+                            echo "<button type='submit' name='cadastrar' id='' class='btn btn-primary '>
+
+                            Atualizar
+                           <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                                                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325'/></svg>
+                        </button>";
+                        }else{
+                            echo "<button type='submit' name='cadastrar' id='' class='btn btn-success '>
 
                             Cadastrar
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus-square' viewBox='0 0 16 16'>
+                                <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z' />
+                                <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4' />
                             </svg>
-                        </button>
+                        </button>";
+                        }
+                        
+                        
+                        
+                        
+                        ?>
+                        
                     </div>
                 </form>
             </div>
--- pages/produtos.php
+++ pages/produtos.php
@@ -94,7 +94,7 @@ if (!empty($_GET['deletar'])) {
                                         <td>" . $result['preco_venda'] . "</td>
                                         <td>" . $result['saldo_estoque'] . "</td> 
                                         <td class='text-center'>
-                                            <a class='btn btn-primary action-button' data-id='" . $result['id'] . "' href='atualizarProduto.php'>
+                                            <a class='btn btn-primary action-button' data-id='" . $result['id'] . "' href='cadastroProduto.php?id=" . $result['id'] . "&nome=" . $result['nome'] . " &cod_categoria=" . $result['id_categoria'] . "&custo=" . $result['preco_custo'] . "&venda=" . $result['preco_venda'] . "&estoque=" . $result['saldo_estoque'] . "&codBarra=" . $result['codigo_de_barra'] . "&tipo=" . $result['unidade_de_medida'] . "&categoria=" . $result['categoria_nome'] . "'>
                                                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
                                                 <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325'/></svg>
                                             </a> 
@@ -185,8 +185,6 @@ if (!empty($_GET['deletar'])) {
         var id = document.getElementById('deleteButton').getAttribute('data-id'); // Recupera o ID salvo
         window.location = 'produtos.php?deletar=' + id;
     }
-
-    
 </script>
 
 </html>
